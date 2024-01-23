6dbd6d9b914d053493c256463146aea5edd63ae1
davisdavid <david_davis1@alumni.baylor.edu>
2024-01-23 14:48:50 -0600

    added basic files to run project locally

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Header title="Welcome to My Portfolio" />
+      <p>This is the home page content.</p>
+    </div>
+  );
+};
+
+export default Home;
