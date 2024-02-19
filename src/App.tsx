5401e6531d51b43efb8dfc538affa667122cf73e
davisdavid <david_davis1@alumni.baylor.edu>
2024-02-19 15:25:54 -0600

    added configurations and initialized creating header process

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Header from './components/Header'
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home title='Home' />} />
-        <Route path='/projects' element={<Home title='Project' />} />
-        <Route path='/about' element={<Home title='About' />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Header/>
+			<Routes>
+				<Route path='/' element={<Home/>} />
+				<Route path='/projects' element={<Home/>} />
+				<Route path='/about' element={<Home/>} />
+			</Routes> 
+		</Router>
+	);
 };
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+	theme: {
+    fontFamily: {
+      'serif': ['Georgia', 'serif'],
+    },
+		extend: {
+      colors: {
+        primary: '#6B7D49',
+        secondary: '#f7f7f7',
+        tertiary: '#92A572'
+      },
+      textColor: {
+        'default': '#565656'
+      }
+    },
+	},
+	plugins: [],
+}
+
