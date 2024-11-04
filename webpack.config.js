4cb2c829160cf865a7a336b6906115f86fff3114
davisdavid <david_davis1@alumni.baylor.edu>
2024-11-04 11:19:07 -0600

    updated settings

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/personal-portfolio/',
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
