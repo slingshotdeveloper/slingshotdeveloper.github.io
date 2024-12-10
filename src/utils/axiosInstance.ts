2216a6086490b5e12e024f4f0447b181fa01623d
slingshotdeveloper <slingshotdevelop@gmail.com>
2024-12-10 14:09:11 -0600

    updated axios url to new service

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://personal-portfolio-api-df40.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
